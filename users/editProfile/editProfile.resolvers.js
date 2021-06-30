9441b79f1956c4dc8f0b2eacc57ca26b9b66dec0
Guiwoo Park <guiwoopark@Guiwooui-MacBookAir.local>
2021-06-30 14:39:32 +0900

    editProfile_UpdateProfile_notFixId

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,39 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, userName, email, password: newPassword }
+    ) => {
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          // will handle the id later!
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          userName,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile!",
+        };
+      }
+    },
   },
 };
