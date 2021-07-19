f5f79e45e5069961d1ac5f3e50849eb38db9d459
Guiwoo <park.guiwoo@hotmail.com>
2021-07-19 18:20:46 +0900

    Done
    Backend is done

--- message/roomUpdate/roomUpdate.resolvers.js
+++ message/roomUpdate/roomUpdate.resolvers.js
@@ -1,10 +1,53 @@
+import { withFilter } from "graphql-subscriptions";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constant";
 import pubsub from "../../pubsub";
 
 export default {
   Subscription: {
     roomUpdate: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findFirst({
+          where: {
+            id: args.id,
+            users: {
+              some: {
+                id: context.loggedInUser.id,
+              },
+            },
+          },
+          select: {
+            id: true,
+          },
+        });
+        if (!room) {
+          throw new Error("You shall not see this");
+        }
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          async ({ roomUpdate }, { id }, { loggedInUser }) => {
+            if (roomUpdate.roomId === id) {
+              const room = await client.room.findFirst({
+                where: {
+                  id,
+                  users: {
+                    some: {
+                      id: loggedInUser.id,
+                    },
+                  },
+                },
+                select: {
+                  id: true,
+                },
+              });
+              if (!room) {
+                return false;
+              }
+              return true;
+            }
+          }
+        )(root, args, context, info);
+      },
     },
   },
 };
